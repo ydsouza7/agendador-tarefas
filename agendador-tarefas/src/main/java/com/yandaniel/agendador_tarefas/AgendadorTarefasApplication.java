3e86487df0cebb3901a43434bb1ff435c337a187
Yan <eng.ydsouza@gmail.com>
2026-03-23 22:31:17 -0300

    commit inicial

---
+++ agendador-tarefas/src/main/java/com/yandaniel/agendador_tarefas/AgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.yandaniel.agendador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTarefasApplication.class, args);
+	}
+
+}
---
+++ agendador-tarefas/src/test/java/com/yandaniel/agendador_tarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yandaniel.agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
