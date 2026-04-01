5388f49c8d1b73a2d028709f71f09458077419b3
Yan <eng.ydsouza@gmail.com>
2026-04-01 18:24:52 -0300

    adicionadas configurações de segurança

---
+++ src/main/java/com/yandaniel/agendador_tarefas/business/dto/UsuarioDTO.java
@@ -0,0 +1,18 @@
+package com.yandaniel.agendador_tarefas.business.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+
+}
