b642a89c14cb8f1ee9dfb39c969b7dbf10e98cf4
Yan <eng.ydsouza@gmail.com>
2026-04-20 16:02:52 -0300

    adicionado método post

--- src/main/java/com/yandaniel/agendador_tarefas/infrastructure/entity/TarefasEntity.java
+++ src/main/java/com/yandaniel/agendador_tarefas/infrastructure/entity/TarefasEntity.java
@@ -2,11 +2,13 @@ package com.yandaniel.agendador_tarefas.infrastructure.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.yandaniel.agendador_tarefas.infrastructure.enums.StatusNotificacaoEnum;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
+@Data
 @Document("tarefa")
 public class TarefasEntity {
 
@@ -15,6 +17,7 @@ public class TarefasEntity {
     private String nomeTarefa;
     private String descricao;
     private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime dataEvento;
     private String emailUsuario;
     private LocalDateTime dataAlteracao;
